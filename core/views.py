aa499e5d10ffc5fb4fd4b79e859581ac1bdf4fa7
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-13 13:29:31 +0200

    update post block

--- core/views.py
+++ core/views.py
@@ -7,15 +7,21 @@ from posts.models import Post
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    last_posts = Post.objects.filter(is_active=True).order_by("-updated_at")[:5]
-    most_hot = Post.objects.annotate(
-        comment_count=Count("comments", distinct=True)
-    ).order_by("-comment_count")[:5]
-    help_posts = Post.objects.filter(category_id=1)
-    need_help_posts = help_posts.annotate(
-        comment_count=Count("comments", distinct=True)
-    ).filter(comment_count=0)[:5]
-    user_activities = Comment.objects.all().order_by("-updated_at")[:5]
+    last_posts = Post.objects.filter(is_active=True).order_by("-created_at")[:5]
+    most_hot = (
+        Post.objects.filter(is_active=True)
+        .annotate(comment_count=Count("comments", distinct=True))
+        .order_by("-comment_count")[:5]
+    )
+
+    need_help_posts = (
+        Post.objects.filter(category_id=1, is_active=True)
+        .annotate(comment_count=Count("comments", distinct=True))
+        .filter(comment_count=0)
+        .order_by("-created_at")[:5]
+    )
+
+    user_activities = Comment.objects.filter(is_active=True).order_by("-created_at")[:5]
 
     return render(
         request,
