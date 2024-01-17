d11c04317ba8f177088e745fff46792f854493dd
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-17 16:54:13 +0200

    refactor html_sanitizer

--- core/utils/html_sanitizer.py
+++ core/utils/html_sanitizer.py
@@ -3,13 +3,9 @@ from django.conf import settings
 
 
 def html_sanitizer(content: str) -> str:
-    allowed_tags = getattr(settings, "ALLOWED_TAGS")
-    allowed_attributes = getattr(settings, "ALLOWED_ATTRIBUTES")
-    strip = getattr(settings, "STRIP")
-
     return bleach.clean(
         content,
-        tags=allowed_tags,
-        attributes=allowed_attributes,
-        strip=strip,
+        tags=settings.ALLOWED_TAGS,
+        attributes=settings.ALLOWED_ATTRIBUTES,
+        strip=settings.STRIP,
     )
