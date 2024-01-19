1c5c04287d658b4ee5c6ba7998d1f91e652847d4
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-19 22:17:56 +0200

    post details page

--- comments/forms.py
+++ comments/forms.py
@@ -1,5 +1,3 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
@@ -12,16 +10,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-
-    def __init__(self, *args: tuple, **kwargs: dict) -> None:
-        super(CommentForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper(self)
-        self.helper.form_method = "post"
-        self.helper.layout = Layout(
-            "content",
-            Submit("submit", _("Confirm"), css_class="btn waves-effect waves-light"),
-        )
-        self.field_order = ["content"]
+        labels = {"content": _("Comment")}
 
     def clean_content(self) -> str:
         content = self.cleaned_data.get("content")
