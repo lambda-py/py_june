601156173751b059e0e64aaa209974752859e33b
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-20 11:27:29 +0200

    add 127.0.0.1 to allowed hosts

--- py_june/settings.py
+++ py_june/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.01").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
