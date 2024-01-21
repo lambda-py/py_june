2ab4a0bbd9ddfeefc3aca80d72e08f17f00a6be2
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-21 13:00:10 +0200

    remove version from settings

--- py_june/settings.py
+++ py_june/settings.py
@@ -17,7 +17,6 @@ from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
 load_dotenv()
-VERSION = '0.0.1'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
