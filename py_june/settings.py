ea690c4657d38d14cb71d8bf564f707faada4e60
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-21 12:55:31 +0200

    add bump version

--- py_june/settings.py
+++ py_june/settings.py
@@ -17,6 +17,7 @@ from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
 load_dotenv()
+VERSION = '0.0.1'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
