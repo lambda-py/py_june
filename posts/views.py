44ff902ce58bf0b7217222d832fcd0cfadcf190c
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2024-01-25 19:46:35 +0200

    Pj 103 comment pagination (#21)
    * pj-103-pagination-comment
    
    * pj-103-pagination-comment
    
    * pj-103-pagination-comment
    
    * pj-103-pagination-comment

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
@@ -64,13 +65,20 @@ class DetailsPostView(View):
         comments = Comment.objects.filter(post_id=post.id).order_by("-updated_at")
         form = CommentForm()
 
+        paginator = Paginator(
+            comments, getattr(settings, "COMMENTS_PAGINATION_PER_PAGE")
+        )
+
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
         return render(
             request,
             self.template_name,
             {
                 "post": post,
                 "form": form,
-                "comments": comments,
+                "page_obj": page_obj,
             },
         )
 
