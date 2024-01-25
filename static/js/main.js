6d70c1b9739f17411c63f63c0ad9b415e92a13a7
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2024-01-25 22:27:16 +0200

    pj-130-btn-scrolls-top (#22)
    * pj-130-btn-scrolls-top
    
    * pj-130-btn-scrolls-top
    
    * pj-130-btn-scrolls-top some fix

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,22 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+// Button scroll to top
+let button = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function (){
+    showHideScrollTopBtn();
+};
+function showHideScrollTopBtn() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
