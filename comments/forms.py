8f5aa6cfb06b8b437c9be4d4339b6dbfe1c5a5ad
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-27 18:51:31 +0200

    fix rendering ckeditor widgets for several forms

--- comments/forms.py
+++ comments/forms.py
@@ -1,12 +1,19 @@
+import uuid
+
+from ckeditor.widgets import CKEditorWidget
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from core.utils.html_sanitizer import html_sanitizer
-
 from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # change id of the widget to avoid conflicts with other widgets on the page
+        self.fields['content'].widget = CKEditorWidget(attrs={'id': uuid.uuid4()})
+
     class Meta:
         model = Comment
         fields = ["content"]
