e57fb2ae5fe1eca87de1041115238e40a36b3099
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2024-01-27 22:04:30 +0200

    pj-66-reply-post (#19)
    * pj-66-reply-post
    
    * pj-66-reply-post
    
    * pj-66-reply-btn some improve
    
    * refactor getting settings values
    
    * fix pagination inconsistency
    
    * reply post comment
    
    * fix rendering ckeditor widgets for several forms
    
    * style comment reply buttons
    
    * add quoted comment to reply comment form
    
    ---------
    
    Co-authored-by: Vitalii Hrebennikov <webasman@gmail.com>

--- categories/views.py
+++ categories/views.py
@@ -49,8 +49,9 @@ def category_detail(request: HttpRequest, category_slug: str) -> HttpResponse:
         Post.objects.filter(category_id=category.id, is_active=True)
         .annotate(comments_count=Count("comments"))
         .prefetch_related("comments")
+        .order_by("-created_at")
     )
-    paginator = Paginator(posts, getattr(settings, "POSTS_PAGINATION_PER_PAGE"))
+    paginator = Paginator(posts, settings.POSTS_PAGINATION_PER_PAGE)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
