bb0c66e7003ff633506ce5502954590ec874f6b2
Nikita <nikita88575@localhost.localdomain>
2024-02-07 01:41:48 +0200

    autofmt

--- reactions/views.py
+++ reactions/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views import View
@@ -12,7 +12,7 @@ from .models import Reactions
 class ReactionsView(LoginRequiredMixin, View):
     template_name = "posts/post_detail.html"
 
-    def get(self, request: HttpRequest, id:int) -> HttpResponse:
+    def get(self, request: HttpRequest, id: int) -> HttpResponse:
         post = get_object_or_404(Post, pk=id, is_active=True)
         return redirect("posts:details", post_slug=post.slug)
 
