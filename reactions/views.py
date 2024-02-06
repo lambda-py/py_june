988576451842af8fe450b261770046630c5343f5
Nikita <nikita88575@localhost.localdomain>
2024-02-07 01:40:57 +0200

    fix

--- reactions/views.py
+++ reactions/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.views import View
 
@@ -11,6 +11,10 @@ from .models import Reactions
 
 class ReactionsView(LoginRequiredMixin, View):
     template_name = "posts/post_detail.html"
+
+    def get(self, request: HttpRequest, id:int) -> HttpResponse:
+        post = get_object_or_404(Post, pk=id, is_active=True)
+        return redirect("posts:details", post_slug=post.slug)
 
     def post(self, request: HttpRequest, id: int) -> HttpResponseRedirect:
         post = get_object_or_404(Post, pk=id, is_active=True)
