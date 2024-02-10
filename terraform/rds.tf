27602a81f6d152af7b6516e25203afa5c1e0edf7
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-11 00:20:00 +0200

    fix params group name

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "django_db" {
   db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.postgres13"
+  parameter_group_name = "default.postgres15"
 
   # Network & Security
   vpc_security_group_ids = [aws_security_group.rds_sg.id]  # Security group for RDS
