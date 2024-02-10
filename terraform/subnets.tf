0574c8644e6ba1ebcc8470487fe4ad831a353255
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-10 22:22:09 +0200

    add additional subnet to rds

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -10,10 +10,21 @@ resource "aws_subnet" "django_public_subnet" {
   }
 }
 
+resource "aws_subnet" "rds_subnet_1" {
+  vpc_id            = aws_vpc.django_vpc.id
+  cidr_block        = var.public_subnet_cidrs[0]
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    Name = "RDS Subnet 1"
+  }
+}
+
+
 # RDS Subnet Group
 resource "aws_db_subnet_group" "rds_subnet" {
   name       = "rds_subnet_group"
-  subnet_ids = [aws_subnet.django_public_subnet.id]
+  subnet_ids = [aws_subnet.django_public_subnet.id, aws_subnet.rds_subnet_1.id]
 
   tags = {
     Name = "MyDBSubnetGroup"
