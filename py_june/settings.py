efd5c50e84b00fa7d3c31234b53dff620cb1fde7
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-10 21:18:52 +0200

    add static root

--- py_june/settings.py
+++ py_june/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
