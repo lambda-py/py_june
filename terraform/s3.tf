ea826ecaedb67f238d2d19ec1b520fab67cdd908
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-18 18:50:44 +0200

    block public access to s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,17 @@
 resource "aws_s3_bucket" "bucket" {
   bucket     = "s3-logs"
-  bucket_acl = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
