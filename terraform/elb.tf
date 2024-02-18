caf7fdd53e56420d7a8bf7ec0d9028f48a098d73
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-18 13:52:17 +0200

    add elb

---
+++ terraform/elb.tf
@@ -0,0 +1,29 @@
+resource "aws_elb" "django_elb" {
+  name               = "django-elb"
+  availability_zones = var.availability_zones
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.django_app.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "DjangoELB"
+  }
+}
