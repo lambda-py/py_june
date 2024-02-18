e93a5457281972c33c5c79cf461cdfbcc2d599de
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-18 22:10:02 +0200

    fix mapping public id for private subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "django_private_subnet" {
   vpc_id            = aws_vpc.django_vpc.id
   cidr_block        = var.private_subnet_cidrs[0]
   availability_zone = var.availability_zones[0]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "DjangoPrivateSubnet"
@@ -37,6 +37,7 @@ resource "aws_subnet" "rds_private_subnet" {
   vpc_id            = aws_vpc.django_vpc.id
   cidr_block        = var.private_subnet_cidrs[1]
   availability_zone = var.availability_zones[1]
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "RDSPrivateSubnet"
