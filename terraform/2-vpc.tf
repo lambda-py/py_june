5c47083c50637271e74ee3fbb8306286b07031af
Vitalii Hrebennikov <webasman@gmail.com>
2024-02-25 08:43:46 +0200

    use cidr without variable

--- terraform/2-vpc.tf
+++ terraform/2-vpc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "django_vpc" {
-  cidr_block           = var.vpc_cidr
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "DjangoVPC"
   }
