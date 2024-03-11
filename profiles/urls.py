4d854292f009c65451c53ada39583362e45b6b64
Nikita <nikita88575@localhost.localdomain>
2024-03-11 22:04:35 +0200

    Remaked profile page and added possibility edit profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ProfileView
+from .views import EditProfileView, ProfileView
 
 app_name = "profile"
 
 urlpatterns = [
     path("<slug:profile>/", ProfileView.as_view(), name="profile"),
+    path(
+        "<slug:profile>/edit-profile/", EditProfileView.as_view(), name="edit-profile"
+    ),
 ]
