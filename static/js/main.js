f0f8d12ca7b14703f2224be1c98327e1ab7bf28d
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2024-04-20 14:01:13 +0300

    smooth scroll to top (#105)

--- static/js/main.js
+++ static/js/main.js
@@ -23,6 +23,8 @@ function showHideScrollTopBtn() {
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
