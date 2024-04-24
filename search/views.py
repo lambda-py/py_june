55df4bf8eecc7758b5f10495e5ac77758d953481
NykoEugen <nyko.eugen@gmail.com>
2024-04-24 14:10:10 +0300

    dynamic search in header

--- search/views.py
+++ search/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from posts.models import Post
 
 
 def search(request: HttpRequest) -> HttpResponse:
-    search_text = request.GET.get("q", "")
-
-    query = Post.objects.filter(title__icontains=search_text).order_by("-updated_at")
-    return render(request, "search/search_result.html", {"query": query})
+    search_text = request.GET.get("search", "")
+    if len(search_text) == 0 or not search_text[0].isalpha():
+        query = None
+    else:
+        query = Post.objects.filter(title__icontains=search_text).order_by("-updated_at")
+    return render(request, "ui/header/search_results.html", {"query": query})
