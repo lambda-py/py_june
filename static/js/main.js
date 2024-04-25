99566eaf3ede266a0780b4f4250a7b1090dc138e
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2024-04-25 19:38:52 +0300

    dynamic search in header (#106)
    * dynamic search in header
    
    * dynamic search in header
    
    * some improve search
    
    * i18n
    
    * css
    
    * css

--- static/js/main.js
+++ static/js/main.js
@@ -28,3 +28,20 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+
+// Search line processing
+let searchResultsHeader = document.getElementById("search-results-header");
+let searchResults = document.getElementById("search-results");
+let searchInput = document.getElementById("search-input");
+document.addEventListener("click", function (event) {
+
+    if (event.target !== searchInput && !searchResultsHeader.contains(event.target)) {
+        searchResults.style.display = "none";
+    }
+});
+
+searchInput.addEventListener("click", function (event) {
+    if (searchResults.style.display === "none") {
+        searchResults.style.display = "";
+    }
+});
