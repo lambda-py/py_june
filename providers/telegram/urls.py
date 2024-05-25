9f90cbdf11667e611f042356fafa09d30e92eb5b
NykoEugen <nyko.eugen@gmail.com>
2024-05-25 13:19:15 +0300

    telegram auth test

--- providers/telegram/urls.py
+++ providers/telegram/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
-from .views import TelegramOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from django.urls import include, path
+
+from .views import TelegramOAuth2Adapter
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
 
 urlpatterns += default_urlpatterns(TelegramOAuth2Adapter)
--- providers/telegram/views.py
+++ providers/telegram/views.py
@@ -1,13 +1,13 @@
+import requests
 from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
 from allauth.socialaccount.providers.telegram.provider import TelegramProvider
-import requests
 
 
 class TelegramOAuth2Adapter(OAuth2Adapter):
     provider_id = TelegramProvider.id
-    access_token_url = 'https://oauth.telegram.org/token'
-    authorize_url = 'https://oauth.telegram.org/auth'
-    profile_url = 'https://api.telegram.org/bot{token}/getMe'
+    access_token_url = "https://oauth.telegram.org/token"
+    authorize_url = "https://oauth.telegram.org/auth"
+    profile_url = "https://api.telegram.org/bot{token}/getMe"
 
     def complete_login(self, request, app, token, response):
         extra_data = response.json()
@@ -22,6 +22,6 @@ class TelegramOAuth2Adapter(OAuth2Adapter):
     def get_authorize_url(self, request, app):
         authorize_url = self.authorize_url
         bot_id = app.client_id
-        origin = request.build_absolute_uri('/')
-        return_to = request.build_absolute_uri('/accounts/telegram/login/callback/')
+        origin = request.build_absolute_uri("/")
+        return_to = request.build_absolute_uri("/accounts/telegram/login/callback/")
         return f"{authorize_url}?bot_id={bot_id}&origin={origin}&return_to={return_to}&request_access=write&embed=0"
